11cfe047631691579d3893b65f332d1e4651d59c
Kimmy Park-Wolmarans <kimmy.park820@gmail.com>
2020-09-26 00:00:21 -0500

    add some heroku stuff

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+      !request.xhr? && request.format.html?
+    end
 end
