6c9855335f3537d44a1c3b230431109b2beba325
Kimmy Park-Wolmarans <kimmy.park820@gmail.com>
2020-09-28 09:34:01 -0500

    minor changes

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,20 +1,20 @@
 class ChargesController < ApplicationController
-    def create
-        Stripe.api_key = ENV['STRIPE_SECRET_KEY']
+    # def create
+    #     Stripe.api_key = ENV['STRIPE_SECRET_KEY']
 
-        order = Order.find(params[:orderId])
-        amount = 
+    #     order = Order.find(params[:orderId])
+    #     amount = 
 
-        charge = Stripe::Charge.create(
-            :amount => params[:amount],
-            :description => 'OSNY Vintage',
-            :currency => 'usd',
-            :source => params[:token]
-        )
+    #     charge = Stripe::Charge.create(
+    #         :amount => params[:amount],
+    #         :description => 'OSNY Vintage',
+    #         :currency => 'usd',
+    #         :source => params[:token]
+    #     )
 
-        rescue Stripe::CardError => e
-            flash[:errors] => e.message
-            redirect_to charges_path
-        end
-    end
+    #     rescue Stripe::CardError => e
+    #         flash[:errors] => e.message
+    #         redirect_to charges_path
+    #     end
+    # end
 end
--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,7 +4,10 @@ function Footer() {
     return (
         <footer>
             <div className='color'></div>
-            <h4>OSNY, 2020</h4>
+            <div className='footer-content'>
+                <h4>OSNY, 2020</h4>
+                <a href='https://www.instagram.com/oldsouls_newyou/' target='_blank'><img className='footer-socials' src='https://www.iconfinder.com/data/icons/picons-social/57/38-instagram-3-512.png' alt='social media logo'/></a>
+            </div>
         </footer>
     )
 }
